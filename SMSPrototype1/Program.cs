61ec98825272071ab05eddccb53548197e42b447
VinishaRathod <er.vinisharathod@gmail.com>
2025-05-10 00:24:52 +0530

    commit

--- SMSPrototype1/Program.cs
+++ SMSPrototype1/Program.cs
@@ -1,24 +1,7 @@
 //Radhe Radhe Anu 
 namespace SMSPrototype1
 {
-    //jisme change kiya h use stage kru?
-    // jse ki prgrm.cs
-    // jese hi change kregi vo autometically change me aa jayegiok
-    // jb bhi daily kam pura ho jaye ya task pura ho jaye or teko lge ab merge krnah
-    // tb ye sb krna dekh
-    //after pushing code
-
-    // Steps : -
-    // 1. main ranch me kuch bhi change krne ke liye pehle vini branch select kr
-    // 2. usme fetch and pull kr
-    // 3. jo change krna h vo change kr
-    // 4. message likh or pehle stage kr use
-    // 5. Mesage likh commit ka or Push kr de
-    //    d
-
-
-
-
+    
     public class Program
     {
         public static void Main(string[] args)
