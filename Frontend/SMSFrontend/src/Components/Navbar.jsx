04377d361da8d0028bc0871a66611efd499a8711
Anand Sharma <anandsharma.ts3000@gmail.com>
2025-06-15 01:55:11 +0530

    Frontend Update

---
+++ Frontend/SMSFrontend/src/Components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/Photos/logo.png'
+import SchoolLogo from '../assets/Photos/SchoolLogo.png'
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav className="bg-transparent w-full relative sticky top-0 z-50">
+            <div className='absolute top-0 left-1/2 transform -translate-x-1/2 w-[98%] h-[100%] bg-white opacity-40 z-0 rounded-lg'></div>
+            <div className='w-[98%] mx-auto z-20 border-b border-white border-opacity-90 rounded-lg relative'>
+                <div className="mx-auto px-4 py-3 flex flex-col md:flex-row justify-between items-center">
+                    <div className="flex justify-between items-center w-full">
+                        <NavLink to="/" className="flex items-center text-xl font-bold">
+                            {/* <img src={SchoolLogo} alt="Logo" className=" h-10 mr-2" /> */}
+                            <img src={logo} alt="Logo" className=" h-10 mr-2" />
+                            <div className='text-[28px] text-black'>AV SMS</div>
+                        </NavLink>
+                        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+                            <span className="block w-6 h-0.5 bg-black mb-1"></span>
+                            <span className="block w-6 h-0.5 bg-black mb-1"></span>
+                            <span className="block w-6 h-0.5 bg-black"></span>
+                        </button>
+                    </div>
+                    <div className={`flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-12 mr-4 text-left w-full md:w-auto ${isOpen ? 'block' : 'hidden'} md:block`}>
+                        <NavLink to="/about" className="text-black hover:text-gray-600 text-[20px]">About</NavLink>
+                        <NavLink to="/pricing" className="text-black hover:text-gray-600 text-[20px]">Pricing</NavLink>
+                        <NavLink to="/schools" className="text-black hover:text-gray-600 text-[20px]">Schools</NavLink>
+                        <NavLink to="/login" className="text-black hover:text-gray-600 text-[20px]">LogIn/SignUp</NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
--- Frontend/SMSFrontend/src/Pages/Home.jsx
+++ Frontend/SMSFrontend/src/Pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import HomePageBackground from '../assets/Photos/HomePagebackground1.jpg'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className=''>
+      <div className='bg-cover bg-center h-screen' style={{ backgroundImage: `url(${HomePageBackground})` }}>
+        <Navbar />
+        <div className='flex flex-col items-center justify-center h-full'>
+          <h1 className='text-white text-4xl font-bold'>Welcome to SMS</h1>
+          <p className='text-white text-2xl'>The best way to manage your SMS</p>
+        </div>
+      </div>
     </div>
   )
 }
