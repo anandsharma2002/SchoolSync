cbe47e1c885a4d7c4177ba23133b25ae7a62859e
VinishaRathod <er.vinisharathod@gmail.com>
2025-06-15 22:54:36 +0530

    Attendance functionaly

---
+++ Backend/SMSDataModel/Model/RequestDtos/CreateAttendanceRqstDto.cs
@@ -0,0 +1,20 @@
+﻿using SMSDataModel.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSDataModel.Model.RequestDtos
+{
+    public class CreateAttendanceRqstDto
+    {
+        public Guid StudentId { get; set; }
+        public Guid ClassId { get; set; }
+        public Guid SchoolId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Status { get; set; }
+
+
+    }
+}
