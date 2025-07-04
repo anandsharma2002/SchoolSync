a2439d773b2c483ab17611f958b2778e2c1f77b2
rohitja26 <jainrohit0002@gamil.com>
2025-07-05 01:15:51 +0530

    Updated-feature

--- Frontend/SMSFrontend/src/App.jsx
+++ Frontend/SMSFrontend/src/App.jsx
@@ -1,29 +1,26 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Pricing from './Pages/Pricing';
-import Schools from './Pages/Schools';
-import Login from './Pages/Login';
-import Read from './Pages/Read';
-import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Pricing from "./Pages/Pricing";
+import Schools from "./Pages/Schools";
+import Login from "./Pages/Login";
+import Read from "./Pages/Read";
+import Navbar from "./Components/Navbar";
 
 // Dashboard Pages
-import Dashboard from './Pages/Dashboard/Dashboard';
-import Teachers from './Pages/Dashboard/Teachers';
-import Students from './Pages/Dashboard/Students';
-import Courses from './Pages/Dashboard/Courses';
-import Schedule from './Pages/Dashboard/Schedule';
-import Reports from './Pages/Dashboard/Reports';
-import Settings from './Pages/Dashboard/Settings';
-import AddStudent from './Pages/Dashboard/AddStudent';
-import AddNewTeacher from './Pages/Dashboard/AddNewTeacher';  
-import UpdateTeacher from './Pages/Dashboard/UpdateTeacher';
-import Class from './Pages/Dashboard/Class';
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Teachers from "./Pages/Dashboard/Teachers";
+import Students from "./Pages/Dashboard/Students";
+import Courses from "./Pages/Dashboard/Courses";
+import Schedule from "./Pages/Dashboard/Schedule";
+import Reports from "./Pages/Dashboard/Reports";
+import Settings from "./Pages/Dashboard/Settings";
+import AddStudent from "./Pages/Dashboard/AddStudent";
+import AddNewTeacher from "./Pages/Dashboard/AddNewTeacher";
+import UpdateTeacher from "./Pages/Dashboard/UpdateTeacher";
+import Class from "./Pages/Dashboard/Class";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Navbar />
@@ -47,10 +44,10 @@ function App() {
         <Route path="/settings" element={<Settings />} />
         <Route path="/addNewTeacher" element={<AddNewTeacher />} />
         <Route path="/updateTeacher" element={<UpdateTeacher />} />
-        <Route path="/students" element={<Students />} /> 
+        <Route path="/students" element={<Students />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
