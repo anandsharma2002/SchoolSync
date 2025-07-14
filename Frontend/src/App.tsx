b3194c5ed26644109be948e8d7b8a63ddd3b0e1b
rohitja26 <jainrohit0002@gamil.com>
2025-07-14 23:56:14 +0530

    fixed the issue of scroll preservance in the next page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -23,70 +22,86 @@ import Settings from "./pages/dashboard/Settings";
 import Announcements from "./pages/dashboard/Announcements";
 import Payment from "./pages/dashboard/Payment";
 import Leave from "./pages/dashboard/Leave";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 const schoolId = "FDB0E272-D728-4AC6-ADEF-00CA12F0AC15";
 
 const App: React.FC = () => (
-  
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           {/* Public routes with navigation */}
-          <Route path="/" element={
-            <div className="min-h-screen flex flex-col">
-              <Navigation />
-              <main className="flex-1">
-                <Home />
-              </main>
-              <Footer />
-            </div>
-          } />
-          <Route path="/about" element={
-            <div className="min-h-screen flex flex-col">
-              <Navigation />
-              <main className="flex-1">
-                <About />
-              </main>
-              <Footer />
-            </div>
-          } />
-          <Route path="/pricing" element={
-            <div className="min-h-screen flex flex-col">
-              <Navigation />
-              <main className="flex-1">
-                <Pricing />
-              </main>
-              <Footer />
-            </div>
-          } />
-          <Route path="/contact" element={
-            <div className="min-h-screen flex flex-col">
-              <Navigation />
-              <main className="flex-1">
-                <Contact />
-              </main>
-              <Footer />
-            </div>
-          } />
-          
+          <Route
+            path="/"
+            element={
+              <div className="min-h-screen flex flex-col">
+                <Navigation />
+                <main className="flex-1">
+                  <Home />
+                </main>
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div className="min-h-screen flex flex-col">
+                <Navigation />
+                <main className="flex-1">
+                  <About />
+                </main>
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/pricing"
+            element={
+              <div className="min-h-screen flex flex-col">
+                <Navigation />
+                <main className="flex-1">
+                  <Pricing />
+                </main>
+                <Footer />
+              </div>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <div className="min-h-screen flex flex-col">
+                <Navigation />
+                <main className="flex-1">
+                  <Contact />
+                </main>
+                <Footer />
+              </div>
+            }
+          />
+
           {/* Dashboard routes without public navigation */}
           <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<DashboardHome schoolId = {schoolId} />} />
-            <Route path="classes" element={<Classes schoolId = {schoolId} />} />
-            <Route path="teachers" element={<Teachers schoolId = {schoolId} />} />
-            <Route path="students" element={<Students schoolId = {schoolId} />} />
-            <Route path="schedule" element={<Schedule schoolId = {schoolId} />} />
-            <Route path="announcements" element={<Announcements schoolId = {schoolId}/>} />
-            <Route path="reports" element={<Reports schoolId = {schoolId} />} />
-            <Route path="settings" element={<Settings schoolId = {schoolId}/>} />
-            <Route path="payment" element={<Payment schoolId = {schoolId}/>} />
-            <Route path="leave" element={<Leave schoolId = {schoolId}/>} />
+            <Route index element={<DashboardHome schoolId={schoolId} />} />
+            <Route path="classes" element={<Classes schoolId={schoolId} />} />
+            <Route path="teachers" element={<Teachers schoolId={schoolId} />} />
+            <Route path="students" element={<Students schoolId={schoolId} />} />
+            <Route path="schedule" element={<Schedule schoolId={schoolId} />} />
+            <Route
+              path="announcements"
+              element={<Announcements schoolId={schoolId} />}
+            />
+            <Route path="reports" element={<Reports schoolId={schoolId} />} />
+            <Route path="settings" element={<Settings schoolId={schoolId} />} />
+            <Route path="payment" element={<Payment schoolId={schoolId} />} />
+            <Route path="leave" element={<Leave schoolId={schoolId} />} />
           </Route>
-          
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
