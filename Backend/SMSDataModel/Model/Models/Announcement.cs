44e1b63b8ac04f2f00ed0b8e5c30e33a4ce39088
Anand Sharma <anandsharma.ts3000@gmail.com>
2025-07-15 23:38:27 +0530

    Updated Models

--- Backend/SMSDataModel/Model/Models/Announcement.cs
+++ Backend/SMSDataModel/Model/Models/Announcement.cs
@@ -8,10 +8,10 @@ namespace SMSDataModel.Model.Models
 {
     public class Announcement
     {
-        public int AnnouncementId { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
-        public string AnnouncementDetail {  get; set; }
-        public DateOnly AnnouncementDate { get; set; }
+        public string Detail {  get; set; }
+        public DateOnly Date { get; set; }
         public string AnnouncedBy {  get; set; }
     }
 }
--- Backend/SMSDataModel/Model/Models/School.cs
+++ Backend/SMSDataModel/Model/Models/School.cs
@@ -9,22 +9,23 @@ namespace SMSDataModel.Model.Models
 {
     public class School
     {
-        public Guid SchoolId { get; set; }
-        public string SchoolName { get; set; }
+        public Guid Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Name { get; set; }
 
-        public string SchoolEmail { get; set; }
+        public string Email { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string Phone { get; set; }
 
         public string Address { get; set; }
 
         public string City { get; set; }
 
         public string State { get; set; }
-
         public int PinCode { get; set; }
         public int Subscription { get; set; } = 0;
         public DateOnly? SubscriptionDate { get; set; }
+        public bool IsSoftDeleted { get; set; }
 
 
     }
